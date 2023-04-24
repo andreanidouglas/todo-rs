6e84619f1092647e0097e9dd09a0ae16cd3d0537
Douglas A <andreani.dr@gmail.com>
2023-04-24 12:09:57 -0300

    feat(simple-todo): add enpoint to post simple todo

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -25,3 +25,50 @@ async fn health_check_should_return_ok() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[tokio::test]
+async fn post_todo_returns_400_if_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("\"completed\": true}", "missing item"),
+        ("{\"item\": \"error test case\"}", "missing completed"),
+        ("{}", "missing item and completed"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/api/todos", &address))
+            .header("content-type", "application/json")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
+}
+
+#[tokio::test]
+async fn post_todo_returns_200_for_valid_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "{\"item\": \"error test case\", \"completed\": true}";
+
+    let response = client
+        .post(&format!("{}/api/todos", &address))
+        .header("content-type", "application/json")
+        .body(body)
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    assert_eq!(200, response.status().as_u16())
+}
